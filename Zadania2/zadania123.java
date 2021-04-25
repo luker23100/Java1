8a762e68d4cfacd1d685600d01bc7daa7c7a4bff
Mikołaj <mikolajjanusz23@gmail.com>
2021-04-25 21:15:15 +0200

    Dodano zadania 1, 2, 3

---
+++ Zadania2/zadania123.java
@@ -0,0 +1,31 @@
+public class zadania123 {
+
+	public static void main(String[] args) {
+		// Zad1
+		int rokUrodzenia = 2001;
+		char plec = 'K';
+		
+		System.out.println("=======Zadanie1=======");
+		System.out.println("Rok urodzenia: " + rokUrodzenia + "\n" + "P³eæ: " + plec);
+		System.out.println("======================");
+		
+		// Zad2
+		boolean czyPelnoletnia = true;
+		float sredniaOcen = 3.5f;
+		
+		System.out.println();
+		System.out.println("=======Zadanie2=======");
+		System.out.println("Czy jest pe³noletnia: " + czyPelnoletnia + "\n" + "Œrednia ocen: " + sredniaOcen);
+		System.out.println("======================");
+	
+		// Zad3
+		byte nrDomu = 12;
+		byte nrMieszkania = 2;
+		float stanKonta = 39456.23f;
+		
+		System.out.println();
+		System.out.println("=======Zadanie3=======");
+		System.out.println("Numer domu: " + nrDomu + "\n" + "Numer mieszkania: " + nrMieszkania + "\n" + "Stan konta: " + stanKonta + " z³");
+		System.out.println("======================");
+	}
+}
