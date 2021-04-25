9590bf85941e1cb1232a3c75fe8c4c143e32c264
Mikołaj <mikolajjanusz23@gmail.com>
2021-04-25 21:25:39 +0200

    Dodano zadania12

---
+++ Zadania3/zadania12.java
@@ -0,0 +1,24 @@
+public class zadania12 {
+
+	public static void main(String[] args) {
+		//Zad1
+		
+		int liczbaPracownikowFirmy = 10420;
+		byte poczatkowaLiczbaPracownikowFirmy = 10;
+		
+		liczbaPracownikowFirmy = poczatkowaLiczbaPracownikowFirmy;
+		System.out.println("=======Zadanie1=======");
+		System.out.println("L. P. Firmy = " + liczbaPracownikowFirmy);
+		System.out.println("======================");
+		
+		//zad2
+		int lMieszkancow;
+		short liczbaMieszkancowZakopanego = 27266;
+		lMieszkancow = liczbaMieszkancowZakopanego;
+		
+		System.out.println();
+		System.out.println("=======Zadanie2=======");
+		System.out.println("L. Mieszkancow = " + lMieszkancow);
+		System.out.println("======================");
+	}
+}
